46a17682eebda00011b5499c76659e2a2fb72468
leonardo <leonardosantana28@yahoo.com.br>
2022-08-05 06:40:06 -0800

    010

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
@@ -14,16 +15,8 @@ use App\Models\Listing;
 |
 */
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //Single Listing
-Route::get('/listings/{listing}', function(Listing $listing) {
-        return view('listing', [
-            'listing' => $listing
-        ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
